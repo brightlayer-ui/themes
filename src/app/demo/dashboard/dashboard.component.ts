9ceafafa3ec320477860d5955d45c772c7c2fe6c
emclaug2 <evan.w.mclaughlin@gmail.com>
2019-09-03 14:33:30 -0400

    Refactored dashboard

--- src/app/demo/dashboard/dashboard.component.ts
+++ src/app/demo/dashboard/dashboard.component.ts
@@ -1,313 +1,68 @@
-import { Component, ChangeDetectorRef , OnInit} from '@angular/core';
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { DomSanitizer } from '@angular/platform-browser';
+import {Component} from '@angular/core';
+import {BreakpointObserver} from '@angular/cdk/layout';
 import * as PXBColors from '@pxblue/colors';
-import {NavigationService} from '../../services/navigation.service';
-import { ActivatedRoute } from '@angular/router';
-
+import {DeviceList} from "./DeviceList";
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  title: any;
-  cols = 4;
-  breakpointSubscription: any;
-  deviceList = [];
+    cols = 4;
+    breakpointSubscription: any;
+    deviceList = DeviceList.getDevices();
 
+    constructor(private breakpointObserver: BreakpointObserver) {
 
-  constructor(
-    private domSanitizer: DomSanitizer,
-    private breakpointObserver: BreakpointObserver,
-    changeDetectorRef: ChangeDetectorRef,
-    private _navigationService: NavigationService,
-    private route: ActivatedRoute
-   ) {
+        this.breakpointSubscription = this.breakpointObserver.observe([
+                '(max-width: 699.99px)',
+                '(max-width: 899.99px)',
+                '(max-width: 1149.99px)'])
+            .subscribe(result => {
+                const small = Object.keys(result.breakpoints)[0];
+                const medium = Object.keys(result.breakpoints)[1];
+                const large = Object.keys(result.breakpoints)[2];
 
-
-
-      this.breakpointSubscription = this.breakpointObserver.observe([ '(max-width: 599.99px)', '(max-width: 959.99px)']).subscribe(result => {
-        const small = Object.keys(result.breakpoints)[0];
-        const medium = Object.keys(result.breakpoints)[1];
-
-        if (result.breakpoints[small]) { this.cols = 1; } else if (result.breakpoints[medium]) { this.cols = 2; } else { this.cols = 4; }
-      });
-
+                if (result.breakpoints[small]) {
+                    this.cols = 1;
+                } else if (result.breakpoints[medium]) {
+                    this.cols = 2;
+                } else if (result.breakpoints[large]) {
+                    this.cols = 3;
+                } else {
+                    this.cols = 4;
+                }
+        });
     }
 
-    onMenuClicked() {
-    this._navigationService.toggleMenu();
-  }
-
-  ngOnInit() {
-    this.title = this.route.snapshot.data.title;
-    this.refreshData();
-  }
-
-  interpretIcon(value) {
-    if (value < 80) {
-      return  'A';
+    interpretIcon(value) {
+        if (value < 80) {
+            return 'A';
+        }
+        if (value < 90) {
+            return 'B';
+        }
+        return 'C';
     }
-    if (value < 90) {
-      return 'B';
+
+    interpretColor(value) {
+        if (value < 25) {
+            return PXBColors.red[500];
+        }
+        if (value < 70) {
+            return PXBColors.yellow[500];
+        }
+        return PXBColors.green[500];
     }
-    return 'C';
-  }
-   interpretColor(value) {
-    if (value < 25) {
-      return PXBColors.red[500];
+
+    gradeInterpretColor(value) {
+        if (value < 80) {
+            return PXBColors.red[500];
+        }
+        if (value < 90) {
+            return PXBColors.yellow[500];
+        }
+        return PXBColors.green[500];
     }
-    if (value < 70) {
-      return PXBColors.yellow[500];
-    }
-    return  PXBColors.green[500];
-  }
-
-     gradeInterpretColor(value) {
-    if (value < 80) {
-      return PXBColors.red[500];
-    }
-    if (value < 90) {
-      return PXBColors.yellow[500];
-    }
-    return  PXBColors.green[500];
-  }
-
-  refreshData() {
-
-
-     this.deviceList =   [{
-          heroValue: 0,
-          loadValue: 20,
-          battery: 12,
-          channels: [{
-              icon: 'wb_sunny',
-              label: 'Temperature',
-              value: 68,
-              unit: '°F'
-            },
-            {
-              icon: 'brightness_high',
-              label: 'Output Voltage',
-              value: 480,
-              unit: 'V'
-            },
-            {
-              icon: 'wb_incandescent',
-              label: 'Output Current',
-              value: 15,
-              unit: 'A'
-            }
-          ]
-      },
-      {
-          heroValue: 100,
-          loadValue: 33,
-          battery: 52,
-          channels: [{
-              icon: 'wb_sunny',
-              label: 'Temperature',
-              value: 68,
-              unit: '°F'
-            },
-            {
-              icon: 'brightness_high',
-              label: 'Output Voltage',
-              value: 480,
-              unit: 'V'
-            },
-            {
-              icon: 'wb_incandescent',
-              label: 'Output Current',
-              value: 15,
-              unit: 'A'
-            }
-          ]
-
-      },
-      {
-          heroValue: 72,
-          loadValue: 98,
-          battery: 98,
-          channels: [{
-              icon: 'wb_sunny',
-              label: 'Temperature',
-              value: 50,
-              unit: '°F'
-            },
-            {
-              icon: 'brightness_high',
-              label: 'Output Voltage',
-              value: 680,
-              unit: 'V'
-            },
-            {
-              icon: 'wb_incandescent',
-              label: 'Output Current',
-              value: 13,
-              unit: 'A'
-            }
-          ]
-      }, {
-
-          heroValue: 54,
-          loadValue: 73,
-          battery: 22,
-          channels: [{
-              icon: 'wb_sunny',
-              label: 'Temperature',
-              value: 78,
-              unit: '°F'
-            },
-            {
-              icon: 'brightness_high',
-              label: 'Output Voltage',
-              value: 250,
-              unit: 'V'
-            },
-            {
-              icon: 'wb_incandescent',
-              label: 'Output Current',
-              value: 20,
-              unit: 'A'
-            }
-          ]
-
-      },
-       {
-
-          heroValue: 54,
-          loadValue: 73,
-          battery: 22,
-          channels: [{
-              icon: 'wb_sunny',
-              label: 'Temperature',
-              value: 78,
-              unit: '°F'
-            },
-            {
-              icon: 'brightness_high',
-              label: 'Output Voltage',
-              value: 250,
-              unit: 'V'
-            },
-            {
-              icon: 'wb_incandescent',
-              label: 'Output Current',
-              value: 20,
-              unit: 'A'
-            }
-          ]
-
-      },
-       {
-
-          heroValue: 54,
-          loadValue: 73,
-          battery: 22,
-          channels: [{
-              icon: 'wb_sunny',
-              label: 'Temperature',
-              value: 78,
-              unit: '°F'
-            },
-            {
-              icon: 'brightness_high',
-              label: 'Output Voltage',
-              value: 250,
-              unit: 'V'
-            },
-            {
-              icon: 'wb_incandescent',
-              label: 'Output Current',
-              value: 20,
-              unit: 'A'
-            }
-          ]
-
-      },
-       {
-
-          heroValue: 54,
-          loadValue: 73,
-          battery: 22,
-          channels: [{
-              icon: 'wb_sunny',
-              label: 'Temperature',
-              value: 78,
-              unit: '°F'
-            },
-            {
-              icon: 'brightness_high',
-              label: 'Output Voltage',
-              value: 250,
-              unit: 'V'
-            },
-            {
-              icon: 'wb_incandescent',
-              label: 'Output Current',
-              value: 20,
-              unit: 'A'
-            }
-          ]
-
-      },
-       {
-
-          heroValue: 54,
-          loadValue: 73,
-          battery: 22,
-          channels: [{
-              icon: 'wb_sunny',
-              label: 'Temperature',
-              value: 78,
-              unit: '°F'
-            },
-            {
-              icon: 'brightness_high',
-              label: 'Output Voltage',
-              value: 250,
-              unit: 'V'
-            },
-            {
-              icon: 'wb_incandescent',
-              label: 'Output Current',
-              value: 20,
-              unit: 'A'
-            }
-          ]
-
-      },
-       {
-
-          heroValue: 54,
-          loadValue: 73,
-          battery: 22,
-          channels: [{
-              icon: 'wb_sunny',
-              label: 'Temperature',
-              value: 78,
-              unit: '°F'
-            },
-            {
-              icon: 'brightness_high',
-              label: 'Output Voltage',
-              value: 250,
-              unit: 'V'
-            },
-            {
-              icon: 'wb_incandescent',
-              label: 'Output Current',
-              value: 20,
-              unit: 'A'
-            }
-          ]
-
-      },
-    ];
-
-  }
-
 }
