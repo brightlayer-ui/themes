da4a3d9917a4a02e21365215a3ee2034f6722730
emclaug2 <evan.w.mclaughlin@gmail.com>
2019-09-03 09:20:06 -0400

    Cycle through themes in an array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,42 @@
-import {Component, Renderer2} from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-    isDark = false;
     showNotice = true;
+
+    themeIndex = 0;
+    themes = ['pxb-blue', 'pxb-blue-dark'];
 
     constructor(private renderer: Renderer2) {}
 
+    ngOnInit(): void {
+        this.applyTheme(this.themes[0]);
+    }
+
     toggleTheme() {
-        this.isDark = !this.isDark;
-        if (this.isDark) {
-          this.renderer.addClass(document.body, 'pxb-blue-dark');
-        } else {
-          this.renderer.removeClass(document.body, 'pxb-blue-dark');
+        const currentTheme = this.themes[this.themeIndex];
+        this.themeIndex++;
+
+        if (this.themeIndex === this.themes.length) {
+            this.themeIndex = 0;
         }
+
+        const newTheme = this.themes[this.themeIndex];
+
+        this.removeTheme(currentTheme);
+        this.applyTheme(newTheme);
+    }
+
+    private applyTheme(theme: string): void {
+        this.renderer.addClass(document.body, theme);
+    }
+
+    private removeTheme(theme: string): void {
+        this.renderer.removeClass(document.body, theme);
     }
 }
