84ed242aa1bbb26b97e5673e547b106418edc198
emclaug2 <evan.w.mclaughlin@gmail.com>
2019-11-18 12:18:36 -0500

    Add these files

---
+++ react/src/index.ts
@@ -0,0 +1,9 @@
+/**
+Copyright (c) 2018-present, Eaton
+ 
+All rights reserved.
+ 
+This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+**/
+export { blueDarkTheme as blueDark } from './blueDarkTheme';
+export { blueTheme as blue } from './blueTheme';
