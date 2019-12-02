35378a41fd722959f30e16402d37532d92779910
emclaug2 <evan.w.mclaughlin@gmail.com>
2019-12-02 09:38:08 -0500

    Remove undeclarated export

--- react-native/src/index.ts
+++ react-native/src/index.ts
@@ -1,8 +1,8 @@
 /**
 Copyright (c) 2018-present, Eaton
- 
+
 All rights reserved.
- 
+
 This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
 **/
-export { blueTheme as blue, expoBlueTheme as expoBlue } from './blueTheme';
+export { blueTheme as blue } from './blueTheme';
