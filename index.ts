46ea25bed8202aba545721ba6f2823cfe5108488
huayunh <hhuayun0828@gmail.com>
2020-01-20 12:15:36 -0500

    try to convert index.js to index.ts to preserve types

---
+++ index.ts
@@ -0,0 +1,18 @@
+/**
+Copyright (c) 2018-present, Eaton
+
+All rights reserved.
+
+This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+**/
+
+import * as RThemes from './react/dist';
+import * as RNThemes from './react-native/dist';
+
+export const ReactThemes = {
+    blue: RThemes.blue,
+    blueDark: RThemes.blueDark
+};
+export const ReactNativeThemes = {
+    blue: RNThemes.blue
+};
