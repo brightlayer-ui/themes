ee12fa71a54fad1c0e0ca7fd9f45b1e92e4afae9
daileytj <daileytj89@gmail.com>
2020-06-03 12:50:22 -0400

    Update react typography margins

--- react/src/shared.ts
+++ react/src/shared.ts
@@ -12,25 +12,52 @@ import { SimplePaletteColorOptions } from '@material-ui/core';
 export const typography = {
     fontFamily: '"Open Sans", Helvetica, Roboto, sans-serif',
     fontWeightMedium: 600,
+    h1: {
+        marginBottom: '8px'
+    },
+    h2: {
+        marginBottom: '8px'
+    },
+    h3: {
+        marginBottom: '8px'
+    },
+    h4: {
+        marginBottom: '8px'
+    },
+    h5: {
+        marginBottom: '8px'
+    },
     h6: {
-        fontWeight: 600
+        fontWeight: 600,
+        marginBottom: '8px'
+    },
+    subtitle1: {
+        marginBottom: '8px'
     },
     subtitle2: {
-        fontWeight: 600
+        fontWeight: 600,
+        marginBottom: '8px'
     },
     body1: {
-        lineHeight: '1.5em'
+        lineHeight: '1.5em',
+        marginBottom: '8px'
     },
     body2: {
-        lineHeight: '1.43em'
+        lineHeight: '1.43em',
+        marginBottom: '8px'
     },
     button: {
         fontWeight: 600,
-        lineHeight: '1.75em'
+        lineHeight: '1.75em',
+        marginBottom: '8px'
+    },
+    caption: {
+        marginBottom: '8px'
     },
     overline: {
         letterSpacing: '2px',
-        fontSize: '12px'
+        fontSize: '12px',
+        marginBottom: '8px'
     },
 
 
