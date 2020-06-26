fe5acd872ad72d08ffc27d30b497897a41951059
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-06-26 10:10:10 -0400

    Change imports

--- angular/demo/src/app/core/material.module.ts
+++ angular/demo/src/app/core/material.module.ts
@@ -1,33 +1,33 @@
 import { NgModule } from '@angular/core';
 import {
-    MatToolbarModule,
-    MatButtonModule,
-    MatListModule,
-    MatCardModule,
-    MatGridListModule,
-    MatIconModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatChipsModule,
-    MatExpansionModule,
-    MatButtonToggleModule,
-    MatCheckboxModule,
-    MatSliderModule,
-    MatTabsModule,
-    MatSlideToggleModule,
-    MatTableModule,
-    MatBadgeModule,
-    MatMenuModule,
-    MatInputModule,
-    MatStepperModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatBottomSheetModule,
-    MatDialog,
     MAT_DIALOG_DEFAULT_OPTIONS,
 } from '@angular/material';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatDialog, MatDialogModule} from "@angular/material/dialog";
+import {MatCardModule} from "@angular/material/card";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSelectModule} from "@angular/material/select";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatBottomSheetModule} from "@angular/material/bottom-sheet";
+import {MatTableModule} from "@angular/material/table";
+import {MatDividerModule} from "@angular/material/divider";
 
 @NgModule({
     exports: [
@@ -56,6 +56,7 @@ import {
         MatNativeDateModule,
         MatBottomSheetModule,
         MatTableModule,
+        MatDividerModule,
     ],
     providers: [MatDialog, { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: true } }],
 })
