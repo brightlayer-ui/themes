ba4963447f56c8396a4d4d55ccbc3a31313025fd
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-09-10 10:18:56 -0400

    Add step to copy theme to the theme demo

--- angular/scripts/linkThemes.sh
+++ angular/scripts/linkThemes.sh
@@ -16,6 +16,7 @@ mkdir "./demos/showcase/node_modules/@pxblue/angular-themes"
 echo -e "${GREEN}Done${NC}"
 
 echo -en "${BLUE}Copying angular themes into node_modules...${NC}";
+# Showcase demo
 cp -r ./demos/showcase/package.json ./demos/showcase/node_modules/@pxblue/angular-themes/package.json
 cp -r ./_blueTheme.scss ./demos/showcase/node_modules/@pxblue/angular-themes/_blueTheme.scss
 cp -r ./_darkTheme.scss ./demos/showcase/node_modules/@pxblue/angular-themes/_darkTheme.scss
@@ -23,7 +24,17 @@ cp -r ./_fonts.scss ./demos/showcase/node_modules/@pxblue/angular-themes/_fonts.
 cp -r ./_margins.scss ./demos/showcase/node_modules/@pxblue/angular-themes/_margins.scss
 cp -r ./_typography.scss ./demos/showcase/node_modules/@pxblue/angular-themes/_typography.scss
 cp -r ./theme.scss ./demos/showcase/node_modules/@pxblue/angular-themes/theme.scss
-cp -r ./pxb-component-theme.scss ./demos/showcase/node_modules/@pxblue/angular-themes/pxb-component-theme.scss
+cp -r ./pxb-component-theme.scss ./demos/showcase/node_modules/@pxblue/angular-themes/pxb-component-theme.
+
+# Theme demo
+cp -r ./demos/showcase/package.json ./demos/theme/node_modules/@pxblue/angular-themes/package.json
+cp -r ./_blueTheme.scss ./demos/theme/node_modules/@pxblue/angular-themes/_blueTheme.scss
+cp -r ./_darkTheme.scss ./demos/theme/node_modules/@pxblue/angular-themes/_darkTheme.scss
+cp -r ./_fonts.scss ./demos/theme/node_modules/@pxblue/angular-themes/_fonts.scss
+cp -r ./_margins.scss ./demos/theme/node_modules/@pxblue/angular-themes/_margins.scss
+cp -r ./_typography.scss ./demos/theme/node_modules/@pxblue/angular-themes/_typography.scss
+cp -r ./theme.scss ./demos/theme/node_modules/@pxblue/angular-themes/theme.scss
+cp -r ./pxb-component-theme.scss ./demos/theme/node_modules/@pxblue/angular-themes/pxb-component-theme.scss
 
 echo -e "${GREEN}Done${NC}"
 
